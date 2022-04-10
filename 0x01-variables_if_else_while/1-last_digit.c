3f8bd6058665d51c20cb5dd647932d066e586ac0
Andy Miguel <miguelandy875@gmail.com>
2022-04-10 21:48:09 +0000

    script to print the last digit of the stored number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+lastdigit = n % 10;
+if (lastdigit > 5)
+{
+printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
+}
+else if (lastdigit == 0)
+{
+printf("last digit of %d is %d and is 0\n", n, lastdigit);
+}
+else if ( lastdigit < 6 && lastdigit != 0)
+{
+printf("last digit of %d is %d and is less than 6 and not 0\n", n);
+}
+	return (0);
+}
+
