6e4b56330e364e0c0be41688bb711fc659826e15
Andy Miguel <miguelandy875@gmail.com>
2023-03-19 09:43:17 -0700

    modified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return:Alway 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
