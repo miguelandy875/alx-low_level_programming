5cdadf45fbc9651e58c50816fe8bc37abc386ca4
Andy Miguel <miguelandy875@gmail.com>
2023-03-19 08:26:33 -0700

    modified: multiple files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,20 @@
-#!/bin/bash
+#include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * return: Alway 0 (success)
+ */
+int main(void)
+{
+	char c;
+	short s;
+	int i;
+	long l;
+
+	print(" The size of Char datatype is %lu byte(s)", sizeof(c));
+	print(" The size of short datatype is %lu byte(s)", sizeof(s));
+	print(" The size of int datatype is %lu byte(s)", sizeof(i));
+	print(" The size of long datatype is %lu byte(s)", sizeof(l));
+	return (0);
+}
