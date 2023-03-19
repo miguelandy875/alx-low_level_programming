f1bd05835f45d51c3b029e9d98434217be7c180f
Andy Miguel <miguelandy875@gmail.com>
2023-03-19 08:50:25 -0700

    modified: 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * return: Alway 0 (success)
+ * Return: Alway 0 (success)
  */
 int main(void)
 {
@@ -12,9 +12,9 @@ int main(void)
 	int i;
 	long l;
 
-	print(" The size of Char datatype is %lu byte(s)", sizeof(c));
-	print(" The size of short datatype is %lu byte(s)", sizeof(s));
-	print(" The size of int datatype is %lu byte(s)", sizeof(i));
-	print(" The size of long datatype is %lu byte(s)", sizeof(l));
+	prinff(" The size of Char datatype is %lu byte(s)\n", sizeof(c));
+	prinf(" The size of short datatype is %lu byte(s)\n", sizeof(s));
+	prinf(" The size of int datatype is %lu byte(s)\n", sizeof(i));
+	prinf(" The size of long datatype is %lu byte(s)\n", sizeof(l));
 	return (0);
 }
