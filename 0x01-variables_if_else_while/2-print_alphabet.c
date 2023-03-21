bfd3456464bcb2a0d0ee371be4d8f1dd98f017bf
Andy Miguel <miguelandy875@gmail.com>
2023-03-20 19:54:19 -0700

    add: multiple files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+		putchar(lcase);
+		putchar('\n');
+	return (0);
+}
