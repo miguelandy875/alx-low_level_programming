d075a58ae5c1436d225440d4e0aeafc4f45416dc
miguel <miguelandy875@gmail.com>
2023-03-21 02:08:18 +0000

    modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-  fprint(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  write(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
   return (1);
 }
 
